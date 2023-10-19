6e847fc16e0eb89b04a41838e064082122626dbf
Andrew Tieszen <pilottzn@gmail.com>
2023-10-19 13:39:59 -0600

    Fix: Increase conditional formatting contrast if too low

--- src/Main/ColorRenderer.java
+++ src/Main/ColorRenderer.java
@@ -10,7 +10,7 @@ import javax.swing.table.TableCellRenderer;
 public class ColorRenderer extends JLabel implements TableCellRenderer {
 	private static final long serialVersionUID = 1L;
 	public ColorRenderer() {
-		setOpaque(true);
+		setOpaque(false);
 	}
 	
 	public Component getTableCellRendererComponent(JTable table, Object value, boolean isSelected, boolean hasFocus,
@@ -20,16 +20,34 @@ public class ColorRenderer extends JLabel implements TableCellRenderer {
 		int warningValue = Settings.getWarningValue();
 		int currentValue = Integer.parseInt(table.getValueAt(row, table.getColumnCount() - 1).toString());
 		
+//		Format cells based on usage
 		if (currentValue <= warningValue)
 			fg = Color.YELLOW;
 		if (currentValue <= warningValue / 2)
 			fg = Color.ORANGE;
 		if (currentValue <= 0)
-			fg = Color.RED;
+			fg = Color.RED.brighter();
 		
-		setBackground(bg);
+//		Increase contrast if too low
+		int contrast = (contrast(bg, fg));
+		if (Math.abs(contrast) < 150) {
+			if (contrast > 0)
+				fg = fg.darker();
+			else
+				fg = fg.brighter();
+		}
+		
+//		
 		setForeground(fg);
 		setText(value.toString());
 		return this;
+	}
+	
+	private int contrast(Color bg, Color fg) {
+		int bgBrightness = bg.getRed() + bg.getGreen() + bg.getBlue() / 3;
+		int fgBrightness = fg.getRed() + fg.getGreen() + fg.getBlue() / 3;
+		
+		int contrast = bgBrightness - fgBrightness;
+ 		return contrast;
 	}
 }
