13b0f3b5dfdc5619a8c990b6662c1f99ee224ac0
Andrew Tieszen <77583314+AJTieszen@users.noreply.github.com>
2024-11-19 14:50:09 -0700

    Create settings.java

---
+++ src/Main/settings.java
@@ -0,0 +1,22 @@
+package Main;
+
+import javax.swing.*;
+
+public class settings {
+	// Settings values
+	private static String db_directory = "%Documents%/FilamentLogs";
+	private static String db_file = "FilamentLog 20xx.accdb";
+	
+	private static Integer student_budget = 500;
+	private static Integer course_budget = 3000;
+	private static Integer course_per_stud = 200;
+	private static Integer warning_value = 50;
+	
+	// Window elements
+	private static JFrame window;
+
+	public static void show() {
+		// Initialize window
+		
+	}
+}
