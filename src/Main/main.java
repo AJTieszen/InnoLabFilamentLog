528f46a21ad51e4346d4ed2ff47ca7da60434c77
Andrew Tieszen <77583314+AJTieszen@users.noreply.github.com>
2024-11-19 14:37:19 -0700

    Update main.java

--- src/Main/main.java
+++ src/Main/main.java
@@ -1,5 +1,14 @@
 package Main;
 
-public class main {
+import java.io.File;
 
+public class main {
+	// Program settings
+	public static final File initFile = new File("3D Print Log.ini");
+	
+	public static void main (String[] args) {
+		if (!initFile.exists()) {
+			System.out.println("No settings file found. Creating...");
+		}
+	}
 }
